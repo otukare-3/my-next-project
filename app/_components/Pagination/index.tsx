94043558a4b24d136dfffdf036bbc73faa002abb
wada.t00 <wada.t00@DTC23024>
2025-02-11 18:13:31 +0900

    7-2-5まで完了

--- app/_components/Pagination/index.tsx
+++ app/_components/Pagination/index.tsx
@@ -4,9 +4,10 @@ import styles from "./index.module.css";
 
 type Props = {
     totalCount: number;
+    current?: number;
 };
 
-export default function Pagination({ totalCount }: Props) {
+export default function Pagination({ totalCount, current = 1 }: Props) {
     const pages = Array.from(
         { length: Math.ceil(totalCount / NEWS_LIST_LIMIT) },
         (_, i) => i + 1
@@ -17,9 +18,13 @@ export default function Pagination({ totalCount }: Props) {
             <ul className={styles.container}>
                 {pages.map((p) => (
                     <li className={styles.list} key={p}>
-                        <Link href={`/news/p/${p}`} className={styles.item}>
-                            {p}
-                        </Link>
+                        {current !== p ? (
+                            <Link href={`/news/p/${p}`} className={styles.item}>
+                                {p}
+                            </Link>
+                        ) : (
+                            <span className={`${styles.item} ${styles.current}`}>{p}</span>
+                        )}
                     </li>
                 ))}
             </ul>
--- app/news/p/[current]/page.tsx
+++ app/news/p/[current]/page.tsx
@@ -27,7 +27,7 @@ export default async function Page({ params }: Props) {
     return (
         <>
             <NewsList news={news} />
-            <Pagination totalCount={totalCount} />
+            <Pagination totalCount={totalCount} current={current} />
         </>
     )
 }
