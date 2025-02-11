20d638cfaa7a9aab037c426567638ad017eeaae2
wada.t00 <wada.t00@DTC23024>
2025-02-11 15:34:46 +0900

    7-2-2まで完了

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,8 +1,12 @@
 import NewsList from "../_components/NewsList";
 import { getNewsList } from "../_libs/microcms";
+import { NEWS_LIST_LIMIT } from "../_constants";
 
 export default async function Page() {
-    const { contents: news } = await getNewsList();
+    const { contents: news } = await getNewsList({
+        limit: NEWS_LIST_LIMIT
+    }
+    );
 
     return <NewsList news={news} />;
 }
