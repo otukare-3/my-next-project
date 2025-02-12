29e879e5d3143e9c774906d5c4e373250cde276f
wada.t00 <wada.t00@DTC23024>
2025-02-12 20:03:44 +0900

    サイト全体にrevalidateを設定する

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -4,8 +4,6 @@ import SearchField from "../_components/SearchField";
 import { NEWS_LIST_LIMIT } from "../_constants";
 import Pagination from "../_components/Pagination";
 
-export const revalidate = 60;
-
 export default async function Page() {
     const { contents: news, totalCount } = await getNewsList({
         limit: NEWS_LIST_LIMIT
--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ import ButtonLink from "./_components/ButtonLink";
 import { getNewsList } from "./_libs/microcms";
 import { TOP_NEWS_LIMIT } from "./_constants";
 
+export const revalidate = 60;
+
 export default async function Home() {
   const data = await getNewsList({
     limit: TOP_NEWS_LIMIT,
