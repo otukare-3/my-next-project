9d6f55b0f4a025828b29de98be576627eb2264be
wada.t00 <wada.t00@DTC23024>
2025-02-12 19:53:22 +0900

    8-4-2まで完了

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -4,6 +4,8 @@ import SearchField from "../_components/SearchField";
 import { NEWS_LIST_LIMIT } from "../_constants";
 import Pagination from "../_components/Pagination";
 
+export const revalidate = 0;
+
 export default async function Page() {
     const { contents: news, totalCount } = await getNewsList({
         limit: NEWS_LIST_LIMIT
